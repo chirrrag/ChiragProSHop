1d800b60bf46e51700a06aa3a4765c9c41143721
Chirrrag <csapra121@gmail.com>
2021-06-03 13:26:09 +0530

    Whole Project npm run dev

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Made with &#10084;&#65039; by Chirag Sapra
+</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
